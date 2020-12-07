e245562a64fb3e418f57fcd3a3a82d43e5266645
CeliaTHP <celia.95190@gmail.com>
2020-12-07 21:10:11 +0100

    imports cleaned

--- app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
